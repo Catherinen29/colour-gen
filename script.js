7b03f6694f34aa5b164e46a7b33f7c40c8baa53d
Catherinen29 <cat_nevin@hotmail.com>
2024-03-23 14:58:54 +0000

    Generate random colours. Set bg colour and text of boxes.

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('Take off!',)
+
+const  hexArray = '0123456789ABCDEF';
+
+// Access each box from HTML
+let box1 = document.querySelector('.colour-1')
+let box2 = document.querySelector('.colour-2')
+let box3 = document.querySelector('.colour-3')
+let box4 = document.querySelector('.colour-4')
+let box5 = document.querySelector('.colour-5')
+
+const boxesArr = [box1, box2, box3, box4, box5]
+
+// Generate a random colour for each box
+const generateColour = () => {
+    for (let b = 0; b < boxesArr.length; b++) {
+        let newColour = '#';
+
+            for (let i = 0; i < 6; i++) {
+                newColour += hexArray[Math.floor(Math.random() * 16)]   
+            }
+
+        boxesArr[b].style.backgroundColor = newColour;
+        boxesArr[b].innerHTML = newColour;
+    }
+}
+
+
+let btn = document.querySelector('.btn-random');
+btn.addEventListener('click', generateColour);
